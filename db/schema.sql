3f8763df86d957950ef1093f683aff2241f3ca26
Chae Jung <chaej_ung@outlook.com>
2020-01-23 21:22:56 -0600

    adding combined files

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,5 @@
-<<<<<<< HEAD
 -- Creates the "banki" database --
 CREATE DATABASE banki;
-=======
--- Drop the banki if it exists--
 
-DROP DATABASE IF EXEISTS banki;
-
---- creats banki database --
-CREAT DATABASE banki;
-USE banki;
-
-CREAT TABLE `banki` (
-
-    `id` Int(11) AUTO_INCREMENT NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `account number` INTEGER (255) NOT NULL,
-    `balance` INTEGER(255) NOT NULL,
-    'email' VARCHAR(255) NOT NULL,
-    `phone#`INTEGER (255) NOT NULL,
-PRIMARY KEY ( `id` )
-);
-
-
-
-
-
-
->>>>>>> d1e4b8584070bee6d63c006e07e173f4d859d17e
+DROP DATABASE IF EXISTS passport_db;
+CREATE DATABASE passport_db;
